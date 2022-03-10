fe2660cbd6ff3e178701c121637010d0078ecb3c
Valentyn <valentyn.postolenko98@gmail.com>
2022-03-10 17:31:11 +0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
 
 function App() {
+  const reducer = (state,action) => {
+    switch (action.type){
+      case 'inc':
+        return {...state, count1: state.count1 + 1}
+      case 'dec':
+        return {...state, count1: state.count1 - 1}
+      case 'res':
+        return {...state, count1: state.count1 = 0}
+    }
+  }
+
+  const [state,dispatch] = useReducer(reducer, {count1: 0})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {state.count1}
+        <button onClick={()=>dispatch({type: 'inc'})}>INC</button>
+        <button onClick={()=>dispatch({type: 'dec'})}>DEC</button>
+        <button onClick={()=>dispatch({type: 'res'})}>RES</button>
+      </div>
   );
 }
 
